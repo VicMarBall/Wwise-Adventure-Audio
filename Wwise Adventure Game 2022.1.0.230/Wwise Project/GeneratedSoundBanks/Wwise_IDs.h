2de697aca5b60ca169bfe128cb478cc1d0c00a8f
Ethanm-0371 <ethanm0371@gmail.com>
2024-05-29 14:58:29 +0200

    Soundbank generation test
    - Removed current build soundbanks
    - Added new generated soundbanks
    - Only added cave ambiance and crystal break sounds

---
+++ Wwise Adventure Game 2022.1.0.230/Wwise Project/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,152 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENT_REGION_CAVE = 1331382810U;
+        static const AkUniqueID DESTRUCTION_CRYSTAL_DESTROY = 448719855U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC_REGIONS
+        {
+            static const AkUniqueID GROUP = 2106907484U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CAVE = 4122393694U;
+                static const AkUniqueID DESERT = 1850388778U;
+                static const AkUniqueID DUNGEON = 608898761U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID NOWHERE = 3424056949U;
+                static const AkUniqueID SILENT = 3160623154U;
+                static const AkUniqueID VILLAGE = 3945572659U;
+                static const AkUniqueID WOODLANDS = 1421251628U;
+                static const AkUniqueID WWIZARDHOUSE = 3132368115U;
+            } // namespace STATE
+        } // namespace MUSIC_REGIONS
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace PLAYER_MOVEMENTSPEED
+        {
+            static const AkUniqueID GROUP = 2471758783U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID RUNNING = 3863236874U;
+                static const AkUniqueID WALKING = 340271938U;
+            } // namespace SWITCH
+        } // namespace PLAYER_MOVEMENTSPEED
+
+        namespace PLAYER_PICKUP_ITEMTYPE
+        {
+            static const AkUniqueID GROUP = 3639591801U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BOOK = 1761501022U;
+                static const AkUniqueID CRYSTALSHARD = 2416868231U;
+                static const AkUniqueID EVILESSENCE = 2979801095U;
+                static const AkUniqueID KEY = 1183067778U;
+                static const AkUniqueID MISC = 3062523257U;
+                static const AkUniqueID MUSHROOM = 1941802987U;
+                static const AkUniqueID PINECONE = 632653906U;
+            } // namespace SWITCH
+        } // namespace PLAYER_PICKUP_ITEMTYPE
+
+        namespace PLAYER_WEAPON_TYPE
+        {
+            static const AkUniqueID GROUP = 3251833556U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID AXE = 765201635U;
+                static const AkUniqueID DAGGER = 3732162827U;
+                static const AkUniqueID HAMMER = 703486095U;
+                static const AkUniqueID PICKAXE = 490825222U;
+                static const AkUniqueID SWORD = 2454616260U;
+            } // namespace SWITCH
+        } // namespace PLAYER_WEAPON_TYPE
+
+        namespace SURFACE_TYPE
+        {
+            static const AkUniqueID GROUP = 4064446173U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID AGENT = 389757836U;
+                static const AkUniqueID BARREL = 442447909U;
+                static const AkUniqueID CRATE = 3098439042U;
+                static const AkUniqueID CRYSTAL = 3444057113U;
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID ENEMY_EVILCRAWLER = 1647104456U;
+                static const AkUniqueID ENEMY_EVILHEAD = 3065846598U;
+                static const AkUniqueID ENEMY_EVILSPITPLANT = 3276966729U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID LEAVES = 582824249U;
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID RUBBLE = 286660663U;
+                static const AkUniqueID SAND = 803837735U;
+                static const AkUniqueID STONE = 1216965916U;
+                static const AkUniqueID WATER = 2654748154U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace SURFACE_TYPE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID COMBATLEVEL = 3717892141U;
+        static const AkUniqueID ENEMY_EVILHEAD_MOVEMENTSPEED = 3046587051U;
+        static const AkUniqueID ENEMY_MUSIC_EVILCRAWLER = 2322571464U;
+        static const AkUniqueID ENEMY_MUSIC_EVILHEAD = 691349574U;
+        static const AkUniqueID ENEMY_MUSIC_EVISPITPLANT = 3481128293U;
+        static const AkUniqueID INMENU_LOWPASS = 1756231317U;
+        static const AkUniqueID MAGIC_CHARGEINTENSITY = 1744050084U;
+        static const AkUniqueID MENUSLIDER_MASTER_VOLUME = 1333239961U;
+        static const AkUniqueID MENUSLIDER_MUSIC_VOLUME = 2642438544U;
+        static const AkUniqueID PLAYER_HEALTH = 215992295U;
+        static const AkUniqueID PLAYER_MOVEMENTSPEED = 2471758783U;
+        static const AkUniqueID TIME_OF_DAY = 207908541U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID MUSIC_GETITEM = 2029038382U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID CAVE = 4122393694U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
